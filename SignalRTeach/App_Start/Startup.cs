46b323f214648c0cfbbfc1acff6fcef899019244
ZhaoKang <ZhaoKang@ZHAOKANG-PC>
2016-11-21 15:01:26 +0800

    添加项目文件。

---
+++ SignalRTeach/App_Start/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace SignalRTeach
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR<EchoConnection>(path: "/realtime/echo");
+        }
+    }
+}
---
+++ SignalRTeach/EchoConnection.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRTeach
+{
+    public class EchoConnection : PersistentConnection
+    {
+       
+
+        protected override async Task OnReceived(IRequest request, string connectionId, string data)
+        {
+            await this.Connection.Broadcast(data);
+        }
+    }
+}
